e0c80a737c3b437e7710d730da87003e49eb0bf9
Brandon Smith <brandon.matthew.smith@gmail.com>
2016-09-02 13:16:01 -0700

    Fix parameter example

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker build -t "apartment-finder:dockerfile" .
-docker run -d -e SLACK_TOKEN={{token}} -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config apartment-finder:dockerfile
+docker run -d -e SLACK_TOKEN={YOUR_SLACK_TOKEN} -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config apartment-finder:dockerfile
 docker ps
