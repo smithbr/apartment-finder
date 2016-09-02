1ee5b840855d8023160f50cbfe01a1d41efa2623
Brandon Smith <brandon.matthew.smith@gmail.com>
2016-09-02 09:28:24 -0700

    Fix template

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-slacktoken=
-
 docker build -t "apartment-finder:dockerfile" .
-docker run -d -e SLACK_TOKEN=$slacktoken -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config apartment-finder:dockerfile
+docker run -d -e SLACK_TOKEN={{token}} -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config apartment-finder:dockerfile
 docker ps
