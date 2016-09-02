f59acfef76cd971a2309feacd1f40e5185d7dd52
Brandon Smith <brandon.matthew.smith@gmail.com>
2016-09-02 01:15:20 -0700

    Fix

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-docker run -d -e SLACK_TOKEN={{put slack token here}} -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config dataquestio/apartment-finder
+slacktoken=
+
+docker build -t "apartment-finder:dockerfile" .
+docker run -d -e SLACK_TOKEN=$slacktoken -v /root/apartment-finder/config:/opt/wwc/apartment-finder/config apartment-finder:dockerfile
 docker ps
